7284c65fb9faca8b5890180a3fb6d3e1bfdf0124
juanjosbg <juanjoseborrero95@gmail.com>
2024-01-16 14:18:16 -0500

    added library three

---
+++ ThreeFiber/src/Components/Scene.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import { Canvas } from '@react-three/fiber';
+
+function Scene() {
+  return (
+    <Canvas>
+      {/* Contenido 3D */}
+    </Canvas>
+  );
+}
+
+export default Scene;
